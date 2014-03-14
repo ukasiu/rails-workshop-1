d66708cfc6ec43653bde875e7ebf0f4c57829cf7
Ukasiu <git@ukasiu.pl>
2014-03-14 15:06:31 +0100

    working server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWorkshop1::Application.config.secret_key_base = '7946d7f34bb9c64cd42670fa07f6afdf2de748001a3bdffc81243bfb0c67b0f55af39693815947657d4ef0473e049a139b129adc1b693e1284e7967c00e12d5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsWorkshop1::Application.config.session_store :cookie_store, key: '_rails-workshop-1_session'
